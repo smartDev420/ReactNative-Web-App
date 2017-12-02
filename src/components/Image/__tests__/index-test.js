necolas/react-native-web

commit 92952ee7460977d79ef27413a351796e80cd927f

[fix] call Image 'onLoad' when image is loaded from cache
Fix #452
Close #712

src/components/Image/index.js
@@ -140,12 +140,18 @@ class Image extends Component {
     this._isMounted = true;
     if (this._imageState === STATUS_PENDING) {
       this._createImageLoader();
+    } else if (this._imageState === STATUS_LOADED) {
+      const { onLoad } = this.props;
+      onLoad && onLoad();
     }
   }
 
   componentDidUpdate() {
     if (this._imageState === STATUS_PENDING) {
       this._createImageLoader();
+    } else if (this._imageState === STATUS_LOADED) {
+      const { onLoad } = this.props;
+      onLoad && onLoad();
     }
   }
 

src/components/Image/__tests__/index-test.js
@@ -1,6 +1,7 @@
 /* eslint-env jasmine, jest */
 
 import Image from '../';
+import ImageLoader from '../../../modules/ImageLoader';
 import ImageUriCache from '../ImageUriCache';
 import React from 'react';
 import { mount, shallow } from 'enzyme';
@@ -153,6 +154,34 @@ describe('components/Image', () => {
   test('prop "testID"', () => {
     const component = shallow(<Image testID="testID" />);
     expect(component.prop('testID')).toBe('testID');
+  });
+
+  describe('prop "onLoad"', () => {
+    test('fires after image is loaded', () => {
+      jest.useFakeTimers();
+      ImageLoader.load = jest.fn().mockImplementation((_, onLoad, onError) => {
+        onLoad();
+      });
+      const onLoadStub = jest.fn();
+      shallow(<Image onLoad={onLoadStub} source="https://test.com/img.jpg" />);
+      jest.runOnlyPendingTimers();
+      expect(ImageLoader.load).toBeCalled();
+      expect(onLoadStub).toBeCalled();
+    });
+
+    test('fires even if the image is cached', () => {
+      jest.useFakeTimers();
+      ImageLoader.load = jest.fn().mockImplementation((_, onLoad, onError) => {
+        onLoad();
+      });
+      const onLoadStub = jest.fn();
+      const uri = 'https://test.com/img.jpg';
+      shallow(<Image onLoad={onLoadStub} source={uri} />);
+      ImageUriCache.add(uri);
+      jest.runOnlyPendingTimers();
+      expect(ImageLoader.load).not.toBeCalled();
+      expect(onLoadStub).toBeCalled();
+    });
   });
 
   test('passes other props through to underlying View', () => {

smartDev420/ReactNative-Web-App

commit 014d63f52ea66d970a41947ff735ada14316b120

[fix] Image 'onLoad' callback on update
'onLoad' should not be called when a component updates, if the 'uri' is
unchanged.

Fixes a regression introduced by
92952ee7460977d79ef27413a351796e80cd927f

src/components/Image/index.js
@@ -149,9 +149,6 @@ class Image extends Component {
   componentDidUpdate() {
     if (this._imageState === STATUS_PENDING) {
       this._createImageLoader();
-    } else if (this._imageState === STATUS_LOADED) {
-      const { onLoad } = this.props;
-      onLoad && onLoad();
     }
   }
 

src/components/Image/__tests__/index-test.js
@@ -87,6 +87,55 @@ describe('components/Image', () => {
     expect(component.find('img').prop('draggable')).toBe(false);
   });
 
+  describe('prop "onLoad"', () => {
+    test('is called after image is loaded from network', () => {
+      jest.useFakeTimers();
+      ImageLoader.load = jest.fn().mockImplementation((_, onLoad, onError) => {
+        onLoad();
+      });
+      const onLoadStub = jest.fn();
+      shallow(<Image onLoad={onLoadStub} source="https://test.com/img.jpg" />);
+      jest.runOnlyPendingTimers();
+      expect(ImageLoader.load).toBeCalled();
+      expect(onLoadStub).toBeCalled();
+    });
+
+    test('is called after image is loaded from cache', () => {
+      jest.useFakeTimers();
+      ImageLoader.load = jest.fn().mockImplementation((_, onLoad, onError) => {
+        onLoad();
+      });
+      const onLoadStub = jest.fn();
+      const uri = 'https://test.com/img.jpg';
+      shallow(<Image onLoad={onLoadStub} source={uri} />);
+      ImageUriCache.add(uri);
+      jest.runOnlyPendingTimers();
+      expect(ImageLoader.load).not.toBeCalled();
+      expect(onLoadStub).toBeCalled();
+      ImageUriCache.remove(uri);
+    });
+
+    test('is called on update if "uri" is different', () => {
+      jest.useFakeTimers();
+      const onLoadStub = jest.fn();
+      const uri = 'https://test.com/img.jpg';
+      const component = mount(<Image onLoad={onLoadStub} source={uri} />);
+      component.setProps({ source: `https://blah.com/img.png` });
+      jest.runOnlyPendingTimers();
+      expect(onLoadStub.mock.calls.length).toBe(2);
+    });
+
+    test('is not called on update if "uri" is the same', () => {
+      jest.useFakeTimers();
+      const onLoadStub = jest.fn();
+      const uri = 'https://test.com/img.jpg';
+      const component = mount(<Image onLoad={onLoadStub} source={uri} />);
+      component.setProps({ resizeMode: 'stretch' });
+      jest.runOnlyPendingTimers();
+      expect(onLoadStub.mock.calls.length).toBe(1);
+    });
+  });
+
   describe('prop "resizeMode"', () => {
     [
       Image.resizeMode.contain,
@@ -154,34 +203,6 @@ describe('components/Image', () => {
   test('prop "testID"', () => {
     const component = shallow(<Image testID="testID" />);
     expect(component.prop('testID')).toBe('testID');
-  });
-
-  describe('prop "onLoad"', () => {
-    test('fires after image is loaded', () => {
-      jest.useFakeTimers();
-      ImageLoader.load = jest.fn().mockImplementation((_, onLoad, onError) => {
-        onLoad();
-      });
-      const onLoadStub = jest.fn();
-      shallow(<Image onLoad={onLoadStub} source="https://test.com/img.jpg" />);
-      jest.runOnlyPendingTimers();
-      expect(ImageLoader.load).toBeCalled();
-      expect(onLoadStub).toBeCalled();
-    });
-
-    test('fires even if the image is cached', () => {
-      jest.useFakeTimers();
-      ImageLoader.load = jest.fn().mockImplementation((_, onLoad, onError) => {
-        onLoad();
-      });
-      const onLoadStub = jest.fn();
-      const uri = 'https://test.com/img.jpg';
-      shallow(<Image onLoad={onLoadStub} source={uri} />);
-      ImageUriCache.add(uri);
-      jest.runOnlyPendingTimers();
-      expect(ImageLoader.load).not.toBeCalled();
-      expect(onLoadStub).toBeCalled();
-    });
   });
 
   test('passes other props through to underlying View', () => {